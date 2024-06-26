8c896d64e00f218581c1221d5d36b820eeae74bb
yamamoto189 <ming1107.000@gmail.com>
2024-06-26 16:56:50 +0900

    add3

---
+++ app/database/migrations/2024_06_26_074649_create_todo_items_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo_items', function (Blueprint $table) {
+            $table->id();
+
+						// ここから追加
+            $table->foreignId('user_id') // 外部キーを追加
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->text('title');
+            $table->boolean('is_done')->default(false);
+            // ここまで追加
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo_items');
+    }
+};
