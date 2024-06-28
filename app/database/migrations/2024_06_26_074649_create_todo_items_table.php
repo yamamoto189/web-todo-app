e4c024f91abede84c399ee0893d8fb67131daf17
yamamoto189 <ming1107.000@gmail.com>
2024-06-28 12:39:51 +0900

    add5

--- app/database/migrations/2024_06_26_074649_create_todo_items_table.php
+++ app/database/migrations/2024_06_26_074649_create_todo_items_table.php
@@ -3,10 +3,11 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
-  /**
+    /**
      * Run the migrations.
      *
      * @return void
